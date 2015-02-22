6c9be6da076ffd09528a44f8c744a15299df8130
Owen Li <owen.li@mail.mcgill.ca>
2015-02-22 04:21:33 -0500

    jscript next picture

--- script.js
+++ script.js
@@ -32,7 +32,11 @@ function scheduleScrollAnimation(){
 
 
 function nextImage(){
-	// TODO
+	$.getJSON("http://www.reddit.com/r/foodporn/.json?jsonp=?", function(data) { 
+    	$.each(data.data.children, function(i,item){
+        	$("<img/>").attr("src", item.data.url).appendTo("#images");
+    	});
+	});
 
 }
 
